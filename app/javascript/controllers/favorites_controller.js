1fc33e50cba1cc0508d2b0593dee8d543241b004
Elias Kirui <eliaskips1@gmail.com>
2022-10-19 22:27:57 +0300

    Favoriting properties

--- app/javascript/controllers/favorites_controller.js
+++ app/javascript/controllers/favorites_controller.js
@@ -1,13 +1,33 @@
 import { Controller } from "@hotwired/stimulus"
+import axios from "axios";
 
 export default class extends Controller {
     favorite() {
+        if (this.element.dataset.userLoggedIn === 'false') {
+            return document.querySelector('[data-header-target="userAuthLink"]').click();
+        }
         if(this.element.dataset.favorited === 'true') {
-            this.element.dataset.favorited = 'false'
-            this.element.setAttribute('fill', 'white');
+            axios.delete(this.element.dataset.unfavoriteUrl, {
+                headers: {
+                    'ACCEPT': 'application/json'
+                }
+            }).then((response) => {
+                this.element.dataset.favorited = 'false'
+                this.element.setAttribute('fill', 'white');
+            })
         } else {
-            this.element.dataset.favorited = 'true'
-            this.element.setAttribute('fill', 'red');
+            axios.post(this.element.dataset.favoriteUrl, {
+                user_id: this.element.dataset.userId,
+                property_id: this.element.dataset.propertyId
+            }, {
+                headers: {
+                    'ACCEPT': 'application/json'
+                }
+            })
+                .then((response) => {
+                    this.element.dataset.favorited = 'true'
+                    this.element.setAttribute('fill', 'red');
+                });
         }
     }
 }
